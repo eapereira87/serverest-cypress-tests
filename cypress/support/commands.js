207a5d11f448d59d3f08f67b35ba670b9a1c11ef
Erick Alves Pereira <ealvesp1987@gmail.com>
2026-03-16 11:52:17 -0300

    test: adiciona comandos para geração de massa dinâmica e apoio à api

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,56 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('generateDynamicUser', (overrides = {}) => {
+  const timestamp = Date.now();
+  const random = Cypress._.random(1000, 9999);
+
+  return cy.wrap({
+    nome: `Erick QA ${timestamp}${random}`,
+    email: `erick.qa.${timestamp}${random}@mail.com`,
+    password: 'Teste@123',
+    administrador: 'true',
+    ...overrides
+  });
+});
+
+Cypress.Commands.add('generateDynamicProduct', (overrides = {}) => {
+  const timestamp = Date.now();
+  const random = Cypress._.random(1000, 9999);
+
+  return cy.wrap({
+    nome: `Produto QA ${timestamp}${random}`,
+    preco: 250,
+    descricao: 'Produto criado via automação Cypress',
+    quantidade: 15,
+    ...overrides
+  });
+});
+
+Cypress.Commands.add('apiCreateUser', (user) => {
+  return cy.request({
+    method: 'POST',
+    url: `${Cypress.env('apiUrl')}/usuarios`,
+    failOnStatusCode: false,
+    body: user
+  });
+});
+
+Cypress.Commands.add('apiLogin', (email, password) => {
+  return cy.request({
+    method: 'POST',
+    url: `${Cypress.env('apiUrl')}/login`,
+    body: {
+      email,
+      password
+    }
+  });
+});
+
+Cypress.Commands.add('createAdminUserViaApi', () => {
+  return cy.generateDynamicUser({ administrador: 'true' }).then((user) => {
+    return cy.apiCreateUser(user).then((response) => {
+      expect(response.status).to.eq(201);
+      expect(response.body.message).to.eq('Cadastro realizado com sucesso');
+
+      return cy.wrap(user);
+    });
+  });
+});
