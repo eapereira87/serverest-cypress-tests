a3339d08d84c1f21ce903704b7fa754578688c23
Erick Alves Pereira <ealvesp1987@gmail.com>
2026-03-16 11:45:06 -0300

    chore: configura baseUrl do frontend e apiUrl da aplicação

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,19 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl: 'https://front.serverest.dev',
+    env: {
+      apiUrl: 'https://serverest.dev'
     },
-  },
+    viewportWidth: 1440,
+    viewportHeight: 900,
+    video: false,
+    defaultCommandTimeout: 10000,
+    requestTimeout: 15000,
+    responseTimeout: 15000,
+    setupNodeEvents(on, config) {
+      return config;
+    }
+  }
 });
