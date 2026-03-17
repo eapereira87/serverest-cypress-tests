95c425e92b699b6f26be63b9d46f81548a97be3c
Erick Alves Pereira <ealvesp1987@gmail.com>
2026-03-17 14:19:49 -0300

    fix: ajusta geração de relatório mochawesome para a pasta real de saída

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,18 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: false,
+    json: true
+  },
   e2e: {
-    baseUrl: 'https://front.serverest.dev',
+    baseUrl: "https://front.serverest.dev",
     env: {
-      apiUrl: 'https://serverest.dev'
+      apiUrl: "https://serverest.dev"
     },
-    viewportWidth: 1440,
-    viewportHeight: 900,
-    video: false,
-    defaultCommandTimeout: 10000,
-    requestTimeout: 15000,
-    responseTimeout: 15000,
     setupNodeEvents(on, config) {
       return config;
     }
